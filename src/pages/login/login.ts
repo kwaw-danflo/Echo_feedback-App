dd0d568c3a3d46682c91aceb8ef8d0b246d289bb
Danflo <danflo@Danflos-MacBook-Pro.local>
2019-03-19 09:30:28 +0000

    added communication placeholder

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -25,7 +25,12 @@ export class LoginPage {
     private loadingControl: LoadingController
     
     ) {}
-
+    ionViewWillEnter(){
+      if ( localStorage.getItem('token')!= null )
+      {
+        this.navCtrl.setRoot(TabsPage)
+      }
+    }
     login(){
       this.showLoading();
       this.auth.login(this.user).then (result => {
